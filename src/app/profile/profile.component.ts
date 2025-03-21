91b338479cb3f0749c6672f549971c554f113b24
fabrown40 <fabrown40@gmail.com>
2025-03-21 22:19:01 +0100

    add profile component and integrate into app

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+
+   selector:'app-profile', 
+   standalone: true,
+   template:"<h1>Profile - Component</h1>"
+})
+
+export class ProfileComponent{
+
+}
