da0d72f2b1e3c299d91183db3046d78f16b90492
fabrown40 <fabrown40@gmail.com>
2025-03-25 23:52:38 +0100

    add profile component with form handling and update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import { ContactComponent } from './contact/contact.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
 import { SignupComponent } from './signup/signup.component';
-import { ProfileComponent } from './profile/profile.component';
 import { UserComponent } from './user/user.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
   { path:'', component:HomeComponent},
@@ -15,7 +15,7 @@ export const routes: Routes = [
   { path: 'signup', component: SignupComponent},
   { path:"user/:id/:name", component:UserComponent},
   { path: 'about', component: AboutComponent },
-  { path: 'profile', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent},
   { path: 'contact', component: ContactComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,28 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-
-   selector:'app-profile', 
-   standalone: true,
-   template:"<h1>Profile - Component</h1>"
+  selector: 'app-profile',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css',
 })
+export class ProfileComponent {
+  name = new FormControl();
+  email = new FormControl();
+  password = new FormControl();
 
-export class ProfileComponent{
+  profileForm = new FormGroup({
+    name1: new FormControl(),
+    email1: new FormControl(),
+    password1: new FormControl(),
+  });
+  submitData(){
+    console.log(this.profileForm.value);
+  }
 
+  getValue() {
+    console.log(this.name.value);
+  }
 }
